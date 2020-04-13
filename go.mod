00c6daaa951499db2f950c80959b7733756dad46
Oscar <oscar.f.larsson@gmail.com>
2020-04-13 11:17:13 +0200

    Added Logrus as default logger

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/golobby/container v1.2.3
 	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/sirupsen/logrus v1.5.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,26 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"iot-home/credentials"
 	"iot-home/endpoints"
 	"iot-home/ioc"
-	"iot-home/logger"
 	"iot-home/netatmo"
 	"net/http"
+	"os"
 
 	"github.com/golobby/container"
 	"github.com/joho/godotenv"
+	logger "github.com/sirupsen/logrus"
 )
 
 var (
 	netatmoService netatmo.Service
 	cred           credentials.CredentialsService
+	port           string = ":3001"
 )
 
 func init() {
+	initLogger()
 	// loads values from .env into the system
 	if err := godotenv.Load(); err != nil {
-		logger.Fatal("No .env file found. Will exit application ...")
+		logger.WithField(".env", "Not found").Fatal("Will exit application ...")
 	}
 
 	ioc.Setup()
@@ -32,19 +36,29 @@ func main() {
 	container.Make(&netatmoService)
 	container.Make(&cred)
 	endpoints.Init(netatmoService)
-	// netatmoRest := new(netatmoRest.Rest)
-	// endpoints.Init(netatmo.New(netatmoRest))
 
 	netatmoCred, err := cred.GetNetatmoOAuth()
 	if err != nil {
-		fmt.Println("wrong: " + err.Error())
+		logger.WithError(err)
 	}
 
-	fmt.Println(netatmoCred)
+	logger.WithFields(logger.Fields{"netatmo cred": netatmoCred}).Info("hello")
 
-	logger.Info("Started web server, listening on :3001 ...")
-	error := http.ListenAndServe(":3001", nil)
+	logger.WithField("Port", port).Info("Started web server ...")
+	error := http.ListenAndServe(port, nil)
 	if error != nil {
-		logger.Fatal(error.Error())
+		logger.WithError(error).Fatal()
 	}
+}
+func initLogger() {
+	file, err := os.OpenFile(
+		"log.txt",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+		0644)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	logger.SetOutput(io.MultiWriter(file, os.Stdout))
 }
